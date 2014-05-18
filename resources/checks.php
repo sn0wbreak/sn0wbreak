effd6d997f19e8a3d083bc8c593788e0f92cd4c1
Daniel+ <admin@Mac-mini.local>
2014-05-18 14:19:25 +0200

    begining of jb script

--- resources/checks.php
+++ resources/checks.php
@@ -1,6 +1,6 @@
 <?php
 $fdir = $_SERVER['HOME'] . "/.sn0wbreak";
-$utils = array("unzip","tar","{$fdir}/ideviceinfo","{$fdir}/partialzip","/os/bin/opensn0w_cli","open","defaults","ssh","bash","php","curl");
+$utils = array("unzip","tar","scp","java","{$fdir}/ideviceinfo","{$fdir}/partialzip","/os/bin/opensn0w_cli","open","defaults","ssh","bash","php","curl");
 foreach($utils as $util)
 {
   echo "Checking for {$util}...";
@@ -8,7 +8,15 @@ $wut = shell_exec("which {$util}");
 if(strlen($wut) == 0)
 {
 echo " \033[31m [ERROR]\033[0m \n";
+if($util == "java")
+{
+echo "Couldn't find java, please install java from java.com\n";
+exit -1;
+}
+else
+{
 die("\033[31m \033[1m Couldn't find {$util}! \033[0m \n");
+}
 }
 else
 {
