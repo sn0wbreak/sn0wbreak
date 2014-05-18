f23f3d367bb42a227abed40fe9f5e3661148e9bf
Daniel+ <admin@Mac-mini.local>
2014-05-18 18:33:29 +0200

    updates

--- resources/init.sh
+++ resources/init.sh
@@ -6,18 +6,19 @@ if [ ! -d $HOME/.sn0wbreak ]
 fi
 cud=`dirname $0`
 sbd="$HOME/.sn0wbreak"
+downloaddomain="localhost"
 if [ ! -f $sbd/partialzip ]
 then
 echo "Downloading partialzip..."
-curl http://files.natur-kultur.eu/pz.zip > $sbd/pz.zip
+curl http://$downloaddomain/pz.zip > $sbd/pz.zip
 unzip $sbd/pz.zip -d $sbd
 rm -rf $sbd/pz.zip
 fi
-if [ ! -f /usr/local/Cellar/libimobiledevice/1.1.5_2/lib/libimobiledevice.4.dylib ] 
+if [ ! -f /usr/local/Cellar/libimobiledevice/1.1.5_2/lib/libimobiledevice.4.dylib ]
   then
   echo "Didn't find the libimobiledevice library in the excepted place in your system, so we'll have to install it, please enter your password when prompted."
   sudo mkdir -p /usr/local/Cellar/libimobiledevice/1.1.5_2/lib/
-$sbd/partialzip http://files.natur-kultur.eu/res.zip libimobiledevice.4.dylib $sbd/libimobiledevice.4.dylib
+$sbd/partialzip http://$downloaddomain/res.zip libimobiledevice.4.dylib $sbd/libimobiledevice.4.dylib
   sudo mv $sbd/libimobiledevice.4.dylib /usr/local/Cellar/libimobiledevice/1.1.5_2/lib/
   echo "Done"
 fi
@@ -25,23 +26,23 @@ if [ ! -f /usr/local/lib/libplist.1.1.10.dylib ]
 then
   echo "Didn't find the libplist library in the excepted place in your system, so we'll have to install it, please enter your password when prompted."
   sudo mkdir -p /usr/local/lib
-$sbd/partialzip http://files.natur-kultur.eu/res.zip libplist.1.1.10.dylib $sbd/libplist.1.1.10.dylib
+$sbd/partialzip http://$downloaddomain/res.zip libplist.1.1.10.dylib $sbd/libplist.1.1.10.dylib
   sudo mv $sbd/libplist.1.1.10.dylib /usr/local/lib/
 echo "Done"
 fi
-if [ ! -f /usr/local/lib/libusbmuxd.2.dylib ] 
+if [ ! -f /usr/local/lib/libusbmuxd.2.dylib ]
   then
   echo "Didn't find the libusbmuxd library in the excepted place in your system, so we'll have to install it, please enter your password when prompted."
   sudo mkdir -p /usr/local/lib
-$sbd/partialzip http://files.natur-kultur.eu/res.zip libusbmuxd.2.dylib $sbd/libusbmuxd.2.dylib
+$sbd/partialzip http://$downloaddomain/res.zip libusbmuxd.2.dylib $sbd/libusbmuxd.2.dylib
   sudo mv $sbd/libusbmuxd.2.dylib /usr/local/lib
   echo "Done"
 fi
-if [ ! -d /os/ ]
+if [ ! -f /os/bin/opensn0w_cli ]
   then
-  echo "Didn't find opensn0w's working directory, installing it, please enter your password when prompted."
+  echo "Didn't find opensn0w's executable, installing it, please enter your password when prompted."
   sudo mkdir /os/
-  $sbd/partialzip http://files.natur-kultur.eu/res.zip os.zip $sbd/os.zip
+  $sbd/partialzip http://$downloaddomain/res.zip os.zip $sbd/os.zip
   sudo unzip $sbd/os.zip -d /
   sudo chown -R $USER /os
 sudo rm -rf $sbd/os.zip
@@ -50,9 +51,41 @@ fi
 if [ ! -f $sbd/ideviceinfo ]
   then
   echo "Downloading ideviceinfo..."
-    $sbd/partialzip http://files.natur-kultur.eu/res.zip ideviceinfo $sbd/ideviceinfo
+    $sbd/partialzip http://$downloaddomain/res.zip ideviceinfo $sbd/ideviceinfo
 chmod 755 $sbd/ideviceinfo
   echo "Done"
+fi
+if [ ! -f $sbd/sshpass ]
+  then
+  echo "Downloading sshpass..."
+    $sbd/partialzip http://$downloaddomain/res.zip sshpass $sbd/sshpass
+chmod 755 $sbd/sshpass
+  echo "Done"
+fi
+if [ ! -f $sbd/ssh_rd.jar ]
+  then
+  echo "Downloading ssh_rd..."
+    $sbd/partialzip http://$downloaddomain/res.zip ssh_rd.jar $sbd/ssh_rd.jar
+  echo "Done"
+fi
+if [ ! -f $sbd/Cydia.tar ]
+  then
+  echo "Downloading cydia bundle..."
+    $sbd/partialzip http://$downloaddomain/res.zip Cydia.tar $sbd/Cydia.tar
+  echo "Done"
+fi
+if [ ! -f $sbd/stether.tar ]
+  then
+  echo "Downloading SemiTether bundle..."
+    $sbd/partialzip http://$downloaddomain/res.zip stether.tar $sbd/stether.tar
+  echo "Done"
+fi
+if [ ! -f $sbd/tar ]
+  then
+  echo "Downloading tar binary..."
+    $sbd/partialzip http://$downloaddomain/res.zip tar $sbd/tar
+    chmod 755 $sbd/tar
+    echo "Done"
 fi
 php $cud/checks.php $sbd
 ex=$(echo $?)
--- resources/jb.sh
+++ resources/jb.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 cud=`dirname $0`
 sbd="$HOME/.sn0wbreak"
+echo "Please make sure your device is connected and in DFU mode, if you haven't done it yet, please identify your device first, before entering DFU mode. Press enter to continue..."
+read NULL
+echo "Starting ssh_rd..."
+java -jar $sbd/ssh_rd.jar > /tmp/rd.log 2>> /tmp/rd.log &
+echo "I'll wait 30 seconds now for ssh_rd..."
+sleep 30
 function executecmd()
 {
 $sbd/sshpass -p alpine ssh root@localhost -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 2022 $@
 }
 echo -n "Waiting for ssh_rd... "
 executecmd echo " [OK]"
+echo -n "Copying tar binray..."
+$sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/tar root@localhost:/bin/
+echo " [OK]"
 echo -n "Copying basic cydia bundle from p0sixspwn..."
 $sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/Cydia.tar root@localhost:/mnt1/
 echo " [OK]"
 echo -n "Extracting bundle..."
-executecmd cd /mnt1 && echo tar xf cydia.tar
+executecmd cd /mnt1 && tar xf cydia.tar
 echo " [OK]"
 echo -n "Removing bundle archive..."
 executecmd rm -rf /mnt1/Cydia.tar
@@ -32,8 +41,7 @@ echo -n "Creating /etc/rc.d..."
 executecmd mkdir /mnt1/etc/rc.d
 echo " [OK]"
 echo -n "Setting nvram variables and rebooting..."
-executecmd nvram auto-boot=true
-reboot_bak
+executecmd nvram auto-boot=true && reboot_bak
 echo " [OK]"
 killall java
 echo "Done"
