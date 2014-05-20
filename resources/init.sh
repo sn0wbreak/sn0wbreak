bcf5500d8a14404eee8d031f9f793c6a86ab1756
Daniel+ <admin@Mac-mini.local>
2014-05-20 18:17:17 +0200

    public url

--- resources/init.sh
+++ resources/init.sh
@@ -6,7 +6,7 @@ if [ ! -d $HOME/.sn0wbreak ]
 fi
 cud=`dirname $0`
 sbd="$HOME/.sn0wbreak"
-downloaddomain="localhost"
+downloaddomain="files.natur-kultur.eu"
 if [ ! -f $sbd/partialzip ]
 then
 echo "Downloading partialzip..."
