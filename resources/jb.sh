3045a0dbfadd5f649d4432b653560035a5a0c716
Daniel+ <admin@Mac-mini.local>
2014-05-20 18:26:32 +0200

    *MUST* work now...

--- resources/jb.sh
+++ resources/jb.sh
@@ -23,7 +23,7 @@ echo -n "Copying basic cydia bundle from p0sixspwn..."
 $sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/Cydia.tar root@localhost:/mnt1/
 echo " [OK]"
 echo -n "Extracting bundle..."
-executecmd "cd /mnt1 && tar xf cydia.tar"
+executecmd "cd /mnt1 && tar xf Cydia.tar"
 echo " [OK]"
 echo -n "Removing bundle archive..."
 executecmd rm -rf /mnt1/Cydia.tar
@@ -37,9 +37,8 @@ echo " [OK]"
 echo -n "Extracting semitether & depencies..."
 executecmd "cd /tmp && tar xf stether.tar && rm stether.tar"
 echo " [OK]"
-executecmd "ls /tmp"
 echo -n "Moving files to their places..."
-executecmd mv /tmp/dirhelper /mnt1/usr/libexec && mv /tmp/mount /mnt1/sbin && mv /tmp/mount_hfs /mnt1/sbin
+executecmd "mv /tmp/dirhelper /mnt1/usr/libexec && mv /tmp/mount /mnt1/sbin && mv /tmp/mount_hfs /mnt1/sbin"
 echo " [OK]"
 echo -n "Creating /etc/rc.d..."
 executecmd mkdir /mnt1/etc/rc.d
