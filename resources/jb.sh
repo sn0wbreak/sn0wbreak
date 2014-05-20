d71317816aacd9f752b7100744e5f912ff762635
Daniel+ <admin@Mac-mini.local>
2014-05-20 18:15:24 +0200

    new bs not tested

--- resources/jb.sh
+++ resources/jb.sh
@@ -13,6 +13,9 @@ $sbd/sshpass -p alpine ssh root@localhost -q -o UserKnownHostsFile=/dev/null -o 
 }
 echo -n "Waiting for ssh_rd... "
 executecmd echo " [OK]"
+echo "Mounting fs..."
+executecmd "mount.sh"
+echo " [ OK]"
 echo -n "Copying tar binray..."
 $sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/tar root@localhost:/bin/
 echo " [OK]"
@@ -20,7 +23,7 @@ echo -n "Copying basic cydia bundle from p0sixspwn..."
 $sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/Cydia.tar root@localhost:/mnt1/
 echo " [OK]"
 echo -n "Extracting bundle..."
-executecmd cd /mnt1 && tar xf cydia.tar
+executecmd "cd /mnt1 && tar xf cydia.tar"
 echo " [OK]"
 echo -n "Removing bundle archive..."
 executecmd rm -rf /mnt1/Cydia.tar
@@ -32,8 +35,9 @@ echo "Copying semitether & depencies..."
 $sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/stether.tar root@localhost:/tmp/
 echo " [OK]"
 echo -n "Extracting semitether & depencies..."
-executecmd cd /tmp && tar xf stether.tar && rm stether.tar
+executecmd "cd /tmp && tar xf stether.tar && rm stether.tar"
 echo " [OK]"
+executecmd "ls /tmp"
 echo -n "Moving files to their places..."
 executecmd mv /tmp/dirhelper /mnt1/usr/libexec && mv /tmp/mount /mnt1/sbin && mv /tmp/mount_hfs /mnt1/sbin
 echo " [OK]"
@@ -41,7 +45,8 @@ echo -n "Creating /etc/rc.d..."
 executecmd mkdir /mnt1/etc/rc.d
 echo " [OK]"
 echo -n "Setting nvram variables and rebooting..."
-executecmd nvram auto-boot=true && reboot_bak
+executecmd nvram auto-boot=true
+executecmd reboot_bak
 echo " [OK]"
-killall java
+php $cud/sshkiller.php
 echo "Done"
