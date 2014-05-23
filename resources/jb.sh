d477ec049aaa0fa43cfbcbeee2f447a78feaa73b
Daniel Troger <daniel@natur-kultur.eu>
2014-05-23 20:35:35 +0200

    nearly working

--- resources/jb.sh
+++ resources/jb.sh
@@ -14,22 +14,28 @@ $sbd/sshpass -p alpine ssh root@localhost -q -o UserKnownHostsFile=/dev/null -o 
 echo -n "Waiting for ssh_rd... "
 executecmd echo " [OK]"
 echo "Mounting fs..."
-executecmd "mount.sh"
-echo " [ OK]"
+executecmd "while [ ! -d /mnt1/etc ]; do mount.sh; done"
+echo " [OK]"
 echo -n "Copying tar binray..."
 $sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/tar root@localhost:/bin/
 echo " [OK]"
-echo -n "Copying basic cydia bundle from p0sixspwn..."
-$sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/Cydia.tar root@localhost:/mnt1/
+echo -n "Copying basic cydia bundle from p0sixspwn, 1/2..."
+$sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/mnt1.tar root@localhost:/mnt1/
 echo " [OK]"
-echo -n "Extracting bundle..."
-executecmd "cd /mnt1 && tar xf Cydia.tar"
+echo -n "Copying basic cydia bundle from p0sixspwn, 2/2..."
+$sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/mnt2.tar root@localhost:/mnt12
 echo " [OK]"
-echo -n "Removing bundle archive..."
-executecmd rm -rf /mnt1/Cydia.tar
+echo -n "Copying ssh..."
+$sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/ssh.tar root@localhost:/mnt1/
 echo " [OK]"
-echo -n "Creating temporary directory..."
-executecmd mkdir /tmp
+echo -n "Uploading fstab..."
+$sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/fstab root@localhost:/mnt1/etc/
+echo " [OK]"
+echo -n "Extracting bundles..."
+executecmd "cd /mnt1 && tar xvf mnt1.tar && tar xvf ssh.tar && cd /mnt2 && tar xvf mnt2.tar"
+echo " [OK]"
+echo -n "Removing old bundles..."
+executecmd "rm -rf /mnt1/mnt1.tar /mnt1/ssh.tar /mnt2/mnt2.tar"
 echo " [OK]"
 echo "Applying semitether..."
 $sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $sbd/dirhelper root@localhost:/mnt1/usr/libexec
--- resources/sshkiller.php
+++ resources/sshkiller.php
@@ -1,7 +1,9 @@
 <?php
 $a = "java -jar " . $_SERVER['HOME'] . "/.sn0wbreak/ssh_rd.jar";
 $b = shell_exec("ps | grep \"{$a}\"");
-$c = explode("\n",$b)[0];
+$c = explode("\n",$b);
+$c = $c[0];
 $d = explode(" ",$c);
 shell_exec("kill {$d[1]}");
+shell_exec("kill {$d[0]}");
 ?>
