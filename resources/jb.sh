eb957b11ff2fb531d0e585a1f0ba67f0f8001077
Spydar007 <spydar007@gmail.com>
2014-05-26 12:58:11 +0100

    Update jb.sh

--- resources/jb.sh
+++ resources/jb.sh
@@ -16,41 +16,41 @@ function transferfile()
 $sbd/sshpass -p alpine scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2022 $1 root@localhost:$2
 }
 echo -n "Waiting for ssh_rd... "
-executecmd echo " [OK]"
+executecmd echo "Done"
 echo "Mounting rootfs..."
 executecmd "while [ ! -d /mnt1/etc ]; do mount.sh; done"
-echo " [OK]"
+echo "Done"
 echo -n "Copying tar binary..."
 transferfile $sbd/tar /bin
-echo " [OK]"
+echo "Done"
 echo -n "Copying cp binary..."
 transferfile $sbd/cp /bin
-echo " [OK]"
+echo "Done"
 echo -n "Copying basic cydia bundle..."
 transferfile $sbd/Cydia.tar /mnt1
-echo " [OK]"
+echo "Done"
 #echo -n "Copying ssh..."
 #transferfile $sbd/ssh.tar /mnt1
 #echo " [OK]"
 echo -n "Uploading fstab..."
 transferfile $sbd/fstab /mnt1/etc
-echo " [OK]"
+echo "Done"
 echo -n "Extracting bundle..."
 executecmd "cd /mnt1 && tar xvf Cydia.tar"
-echo " [OK]"
+echo "Done"
 echo -n "Cleaning up..."
 executecmd "rm -rf /mnt1/Cydia.tar /mnt2/mobile/Library/Caches/com.apple.mobile.installation.plist"
-echo " [OK]"
+echo "Done"
 echo -n "Moving dpkg structure to user partition..."
 executecmd "mv /mnt1/var/* /mnt2"
-echo " [OK]"
+echo "Done"
 echo "Applying Semi-Tether..."
 transferfile $sbd/dirhelper /mnt1/usr/libexec
-echo " [OK]"
+echo "Done"
 echo -n "Setting nvram variables and rebooting..."
 executecmd nvram auto-boot=true
 executecmd reboot_bak
-echo " [OK]"
+echo "Done"
 php $cud/sshkiller.php
 echo "Done, please put your device into DFU mode again and then press Enter to continue..."
 echo > $sbd/jbd
