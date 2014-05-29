5cefa2be0dbfef5fa843a609edbf1ebd2c756855
Daniel Troger <daniel@natur-kultur.eu>
2014-05-29 11:31:10 +0200

    playing with c, just some playground

---
+++ tests/info.c
@@ -0,0 +1,49 @@
+#include <libimobiledevice/libimobiledevice.h>
+#include <libimobiledevice/lockdown.h>
+#include <libimobiledevice/mobile_image_mounter.h>
+#include <libimobiledevice/mobilebackup2.h>
+#include <libimobiledevice/notification_proxy.h>
+#include <libimobiledevice/afc.h>
+#include <libimobiledevice/sbservices.h>
+#include <libimobiledevice/file_relay.h>
+#include <libimobiledevice/diagnostics_relay.h>
+#include <string.h>
+#include <zlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+typedef struct lockdown_t {
+    idevice_t *device;
+    lockdownd_client_t client;
+    } lockdown_t;
+
+struct idevice_private {
+	char *udid;
+	void *conn_data;
+};
+
+
+int main(int argc, char *argv[])
+{
+    idevice_private *device = NULL;
+    char *product = NULL;
+    char *build = NULL; 
+    char *version = NULL;
+    device = device_create(NULL);
+    
+ 
+lockdown_t *lockdown = lockdown_open(device);
+ 
+if ((lockdown_get_string(lockdown, "HardwareModel", &product) != LOCKDOWN_E_SUCCESS)
+        || (lockdown_get_string(lockdown, "BuildVersion", &build) != LOCKDOWN_E_SUCCESS) || (lockdown_get_string(lockdown, "ProductVersion", &version) != LOCKDOWN_E_SUCCESS)) {
+             printf("can't get info about your idevice, pls ! try again\n");
+             lockdown_free(lockdown);
+             device_free(device);
+             return -1;
+}
+             printf("build: %s\n product: %s\nios version:%s\n", build, product,version);
+ 
+             return 0;
+}
