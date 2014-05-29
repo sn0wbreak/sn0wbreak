8e68aae3ee61b3f59514fcaad4cdf2e74a82f860
Daniel+ <admin@Mac-mini.local>
2014-05-29 17:53:52 +0200

    better pyteched

---
+++ tests/test-pytech.c
@@ -0,0 +1,82 @@
+/* Code by sjeezypwn, this is a gift to the sn0wbreak developers
+*/
+
+#include <libimobiledevice/libimobiledevice.h>
+#include <libimobiledevice/lockdown.h>
+#include <libimobiledevice/mobilebackup2.h>
+#include <libimobiledevice/afc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void ERROR(char *errstr)
+{
+    printf("ERROR: %s\n", errstr);
+
+}
+
+void INFO(char *infostr)
+{
+    printf("[*] %s\n", infostr);
+}
+
+
+char *udid;
+idevice_t iDevice = NULL;
+
+int ConnectToDevice()
+{
+    INFO("Connecting to device...");
+
+    if (idevice_new(&iDevice, NULL) != IDEVICE_E_SUCCESS)
+    {
+        ERROR("Cannot connect to device! Make sure it is plugged in.");
+        return -1;
+    }
+    if (idevice_get_udid(iDevice, &udid) != IDEVICE_E_SUCCESS)
+    {
+        ERROR("Can't get UDID of the device. Make sure it is plugged in.");
+        return -1;
+    }
+    return 0;
+}
+
+lockdownd_client_t lockdown = NULL;
+
+int InitLockdown()
+{
+    INFO("Starting lockdown...");
+
+    if (lockdownd_client_new_with_handshake(iDevice, &lockdown, "jalebr4ke") != LOCKDOWN_E_SUCCESS)
+    {
+        ERROR("Could not start lockdown!");
+        return -1;
+    }
+
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (ConnectToDevice() == -1) { return -1; }
+    printf("[*] Successfully connected to the iDevice. UDID: %s\n", udid);
+
+    if (InitLockdown() == -1) { return -1; }
+    printf("[*] Lockdown initialization is sucessful.\n");
+
+    char *product = NULL;
+    char *build = NULL;
+    char *version = NULL;
+
+
+    if ((lockdown_get_string(lockdown, "HardwareModel", &product) != LOCKDOWN_E_SUCCESS)
+            || (lockdown_get_string(lockdown, "BuildVersion", &build) != LOCKDOWN_E_SUCCESS) || (lockdown_get_string(lockdown, "ProductVersion", &version) != LOCKDOWN_E_SUCCESS)) {
+                 printf("can't get info about your idevice, pls ! try again\n");
+                 lockdownd_client_free(lockdown);
+                 device_free(iDevice);
+                 return -1;
+    }
+             printf("build: %s\n product: %s\nios version:%s\n", build, product,version);
+
+             return 0;
+}
