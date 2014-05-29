867413d8c6aacc805a98163736fd1e822f702b94
spawncpy <spawncpy@gmail.com>
2014-05-29 23:28:02 +0200

    Devices
    Device Compatibility stuff

--- tests/info.c
+++ tests/info.c
@@ -1,7 +1,16 @@
 /* Code by sjeezypwn, this is a gift to the sn0wbreak developers
 
 Thanks to PythEch for correcting some typos
+
+And the rest of the sn0wbreak team
+
 */
+
+#include <libimobiledevice/libimobiledevice.h>
+#include <libimobiledevice/afc.h>
+
+#include "device.h"
+#include "lockdown.h"
 
 #include "common.h"
 
@@ -9,81 +18,100 @@ Thanks to PythEch for correcting some typos
 #include <stdlib.h>
 #include <string.h>
 
-int mode = 0;
+typedef struct _compatibility {
+    char *product;
+    char *build;
+} compatibility_t;
 
-#define INFO(x...) \
-    if (mode != 3) { printf("[*] "), printf(x); }
+compatibility_t compatible_devices[] = {
+  {"n90ap", "11A465"}
+  {"n90bap","11A465"}
+  {"n92ap", "11A465"}
+  {"90ap", "11A465"}
+  {"90bap", "11A465"}
+  {"n92ap", "11A465"}
+  {"n90ap", "11B511"}
+  {"n90bap", "11B511"}
+  {"n92ap", "11B511"}
+  {n90ap, }
+  {n90bap, }
+  {n92ap, }
+  {n90ap, }
+  {n90bap, }
+  {n92ap, }
+  {n90ap, }
+  {n90bap, }
+  {n92ap, }
+  {NULL, NULL};
+  
+  //need to finsih this
+}
+
+void INFO(char *infostr)
+{
+  if(strcmp(argv[2], "-q") == 0)
+  {
+    printf("[*] %s\n", infostr);
+  }
+}
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) { return -1; } //avoid crash
-
-    if (strcmp(argv[1], "--boot") == 0)
+  if(strcmp(argv[1], "--boot") == 0)
+  {
+    printf("BOOT!!!");
+  }
+  else // Connects to device
+  {
+    INFO("Connecting to device...");
+    device_t *device = device_create(NULL);
+    if (device == NULL) // Checks if the device is plugged in or not
     {
-        mode = 1;
+        ERROR("Cannot connect to device! Make sure it is plugged in.");
+        return -1;
     }
-    else if (strcmp(argv[1], "--cache") == 0)
-    {
-        mode = 2;
-    }
-    else
-    {
-        mode = 3;
-    }
+    printf("[*] Successfully connected to the iDevice. UDID: %s\n", device->uuid);
 
-    if (mode != 1) // ! --boot
+    INFO("Starting lockdown...");
+
+    lockdown_t *lockdown = lockdown_open(device);   // Startes the lockdown protocol
+    if (lockdown == NULL)
     {
-        INFO("Connecting to device...\n");
-        device_t *device = device_create(NULL);
-        if (device == NULL)
-        {
-            ERROR("Cannot connect to device! Make sure it is plugged in.\n");
-            return -1;
-        }
-        INFO("Successfully connected to the iDevice. UDID: %s\n", device->uuid);
-
-        INFO("Starting lockdown...\n");
-        lockdown_t *lockdown = lockdown_open(device);
-        if (lockdown == NULL)
-        {
-            ERROR("Could not start lockdown!\n");
-            return -1;
-        }
-        INFO("Lockdown initialization is sucessful.\n");
-
-        if (mode == 2) // --cache
-        {
-            char *product = NULL;
-            char *build = NULL;
-            char *version = NULL;
-            if (lockdown_get_string(lockdown, "ProductType", &product) != LOCKDOWN_E_SUCCESS ||
-                lockdown_get_string(lockdown, "BuildVersion", &build) != LOCKDOWN_E_SUCCESS  ||
-                lockdown_get_string(lockdown, "ProductVersion", &version) != LOCKDOWN_E_SUCCESS) 
-            {
-                ERROR("Can't get info about your iDevice, please try again!\n");
-                lockdown_free(lockdown);
-                device_free(device);
-                return -1;
-            }
-            printf("%s_%s_%s\n", product, version, build);
-        }
-        else
-        {
-            char *value = NULL;
-            if (lockdown_get_string(lockdown, argv[1], &value) != LOCKDOWN_E_SUCCESS)
-            {
-                ERROR("Can't get info about your iDevice, please try again!\n");
-                lockdown_free(lockdown);
-                device_free(device);
-                return -1;
-            }
-            printf("%s\n", value);
-        }
+        ERROR("Could not start lockdown!");
+        return -1;
     }
-    else
+    INFO("Lockdown initialization is sucessful.");
+
+    if(strcmp(argv[1], "--cache") == 0)
     {
-        printf("BOOT!\n");
+    char *product = NULL;
+    char *build = NULL;
+    char *version = NULL;
+    if ((lockdown_get_string(lockdown, "ProductType", &product) != LOCKDOWN_E_SUCCESS)
+            || (lockdown_get_string(lockdown, "BuildVersion", &build) != LOCKDOWN_E_SUCCESS)
+            || (lockdown_get_string(lockdown, "ProductVersion", &version) != LOCKDOWN_E_SUCCESS)) {
+                 ERROR("Can't get info about your iDevice, please try again!\n");
+                 lockdown_free(lockdown);
+                 device_free(device);
+                 return -1; // gets Product Type, Build Version and Product version using lockdown
     }
+             printf("%s_%s_%s\n", product, version, build);
+           }
+           else
+           {
+             char *value = NULL;
+             if (lockdown_get_string(lockdown, argv[1], &value) != LOCKDOWN_E_SUCCESS)
+             {
+               ERROR("Can't get info about your iDevice, please try again!\n"); // Couldn't find a device
+               lockdown_free(lockdown);
+               device_free(device);
+               return -1;
+             }
+             printf("%s",value);
+           }
+}
 
-    return 0;
+
+
+             return 0;
 }
