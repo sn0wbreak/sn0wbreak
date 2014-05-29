8d8d989a11a423b278eca2fb0aade425ec594833
Daniel+ <admin@Mac-mini.local>
2014-05-29 17:46:50 +0200

    fixed 2 compilation errors

--- tests/info.c
+++ tests/info.c
@@ -21,21 +21,21 @@ int main(int argc, char *argv[])
 {
     idevice_private *device = NULL;
     char *product = NULL;
-    char *build = NULL; 
+    char *build = NULL;
     char *version = NULL;
     device = device_create(NULL);
-    
- 
+
+
 lockdown_t *lockdown = lockdown_open(device);
- 
+
 if ((lockdown_get_string(lockdown, "HardwareModel", &product) != LOCKDOWN_E_SUCCESS)
         || (lockdown_get_string(lockdown, "BuildVersion", &build) != LOCKDOWN_E_SUCCESS) || (lockdown_get_string(lockdown, "ProductVersion", &version) != LOCKDOWN_E_SUCCESS)) {
              printf("can't get info about your idevice, pls ! try again\n");
-             lockdown_free(lockdown);
-             device_free(device);
+             lockdownd_client_free(lockdown);
+             idevice_free(device);
              return -1;
 }
              printf("build: %s\n product: %s\nios version:%s\n", build, product,version);
- 
+
              return 0;
 }
