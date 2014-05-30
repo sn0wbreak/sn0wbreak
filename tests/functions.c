5cd6b960e8bbd7edd19b9149685aca7449edfcc9
spawncpy <spawncpy@gmail.com>
2014-05-30 16:47:12 +0200

    Update functions.c

--- tests/functions.c
+++ tests/functions.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#define check(file) do { \
-           struct stat lol; \
-	if(stat(path, &lol) != 0) { \
+void check(char *file) 
+           struct stat lol; 
+	if(stat(path, &lol) != 0) { 
 	   exit(0);
-#define check2(file) do { \
-        int i = check(file);				\
-		if(ret) {  \
+	   
+void check2(char *file) 
+        int i = check(file);				
+		if(ret) {  
 		  printf("File doesn't exist")
 
 int main(int argc, char *argv[])
