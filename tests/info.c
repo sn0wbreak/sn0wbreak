3abbf581a1ac8787bda52ab584cf2985872aaf98
Daniel Troger <daniel@natur-kultur.eu>
2014-05-30 19:02:28 +0200

    i think we got dumb again....

--- tests/info.c
+++ tests/info.c
@@ -96,8 +96,8 @@ if(strcmp(argv[2], "-q") == 0)
 }
   if(strcmp(argv[1], "--boot") == 0)
   {
-char homedir[] = getenv("HOME");
-char cache[] = strcat(homedir,"/.sn0wbreak/device_cache");
+char *homedir = getenv("HOME");
+char *cache = strcat(homedir,"/.sn0wbreak/device_cache");
 if(!file_exists(cache))
 {
 printf("Please cache your device first....\n");
@@ -107,9 +107,9 @@ else
 {
 int length = 0; //who the hell cares about it, but it seems like file_get_contents needs it
 char *plistc = NULL;
-char *plist = file_get_contents(cache,&plistc,&length);
+file_get_contents(cache,&plistc,&length);
     INFO("I will boot your device with opensn0w now, with deviceinfos from my cache, please place your device into DFU mode....");
-char *p1 = strcat("/os/bin/opensn0w_cli -p /os/bundles/", plist);
+char *p1 = strcat("/os/bin/opensn0w_cli -p /os/bundles/", plistc);
 printf("%s",p1);
     INFO("Done!\n");
 return 0;
