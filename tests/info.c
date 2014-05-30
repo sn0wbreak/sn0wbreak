f12492f31f5640347ddb8dd92e93b088c80148aa
Daniel Troger <daniel@natur-kultur.eu>
2014-05-30 18:42:35 +0200

    bus error

--- tests/info.c
+++ tests/info.c
@@ -109,12 +109,8 @@ int length = 0; //who the hell cares about it, but it seems like file_get_conten
 char *plistc = NULL;
 char *plist = file_get_contents(cache,&plistc,&length);
     INFO("I will boot your device with opensn0w now, with deviceinfos from my cache, please place your device into DFU mode....");
-char *first = "test";
-char *target = malloc(strlen(plistc) + strlen(first) + 1);
-strcpy(target, first);
-strcat(target, plistc); 
-printf("%s",target);
-free(target); 
+char *p1 = strncat("/os/bin/opensn0w_cli -p /os/bundles/", plistc, ".plist");
+printf("%s",p1);
     INFO("Done!\n");
 return 0;
 }
