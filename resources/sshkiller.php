96c521336dc2fb3d42ce0586419f42644bf74709
danieltroger <daniel@natur-kultur.eu>
2014-07-27 11:24:39 +0200

    Delete sshkiller.php

--- resources/sshkiller.php
+++
@@ -1,9 +0,0 @@
-<?php
-$a = "java -jar " . $_SERVER['HOME'] . "/.sn0wbreak/ssh_rd.jar";
-$b = shell_exec("ps | grep \"{$a}\"");
-$c = explode("\n",$b);
-$c = $c[0];
-$d = explode(" ",$c);
-shell_exec("kill {$d[1]}");
-shell_exec("kill {$d[0]}");
-?>
